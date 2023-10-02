2191c5be5fff49d1db0968402e91e4305f8d9bc1
groovy-tony <jakeboesten@gmail.com>
2023-10-03 08:45:27 +1030

    added update_dropbox function

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from simple_term_menu import TerminalMenu
 import glob
 
 
+db_path = os.environ["HOME"] + "/Dropbox/YT Audio Files/"
+
+
 def draw_menu():
     options = ["Enter a YouTube URL", "Search YouTube", "Enter YouTube Playlist"]
     menu = TerminalMenu(options, title="YouTube Audio Downloader")
@@ -17,7 +20,12 @@ def draw_menu():
             yt_search(input("Enter search query:\n"))
         case 2:
             raise NotImplementedError
-
+    menu = TerminalMenu(
+        ["Yes", "No"], title="Copy downloaded files to Dropbox? ({db_path})"
+    )
+    idx = menu.show()
+    if idx:
+        update_dropbox()
 
 def video_to_audio(url: str):
     yt_dlp_opts = {
@@ -35,9 +43,26 @@ def video_to_audio(url: str):
         error = ydl.download(url)
 
 
-def yt_search(query: str, results=None):
-    results = YoutubeSearch(query, max_results=10).to_dict()
-    titles = [f"{item['channel']} - {item['title']} - {item['duration']} - {item['publish_time']}" for item in results]
+def update_dropbox():
+    # get $HOME path and append Dropbox/YT \Audio file
+    # move all .m4a files to it
+    try:
+        os.mkdir(db_path)
+    except FileExistsError:
+        pass
+    for file in glob.glob("*.m4a"):
+        print(file)
+        os.replace(file, db_path + file)
+
+
+def yt_search(query: str, max_results=None):
+    if max_results is None:
+        max_results = 10
+    results = YoutubeSearch(query, max_results=max_results).to_dict()
+    titles = [
+        f"{item['channel']} - {item['title']} - {item['duration']} - {item['publish_time']}"
+        for item in results
+    ]
     menu = TerminalMenu(titles)
     idx = menu.show()
     selection = results[idx]
@@ -45,5 +70,6 @@ def yt_search(query: str, results=None):
 
 
 if __name__ == "__main__":
+    update_dropbox()
     draw_menu()
     # video_to_audio("https://www.youtube.com/watch?v=jJPchQvTMhw")
